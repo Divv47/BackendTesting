802f8100e98210f7b19f85ee6c1f295b9a11b90a
Divyansh vairagade <116060884+CodeDivyanX@users.noreply.github.com>
2025-03-17 23:17:18 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,77 @@
+import mongoose ,{Schema} from "mongoose";
+import { JsonWebTokenError } from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const userSchema = new Schema({
+    userName :{
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+        index :true,
+    },
+    email:{
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+    },
+    fullName :{
+        type : String,
+        required : true,
+        //unique : true,
+        //lowercase : true,
+        trim : true,
+        index :true,
+    },
+    avatar :{
+        type : String,
+        required : true,
+    },
+    coverImage :{
+        type : String,
+    },
+    watchHistory:[
+        { 
+            type : Schema.Types.ObjectId,
+            ref : "Video"
+        }
+    ],
+    password:{
+        type : String,
+        required : [true,'Password is required'],
+        unique : true,
+    },
+    refreshToken:{
+        type: String,
+    }
+},{
+    timestamps : true,
+})
+
+
+userSchema.pre("save",async function (next,){
+    if(this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10);
+    next()
+})
+
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken = function (
+    jwt.sign({
+        _id: this._id
+        email: this.email
+    })
+){}
+userSchema.methods.generateRefreshToken = function (){}
+
+
+
+
+export const User = mongoose.model("User",userSchema)
---
+++ src/utils/apiError.js
@@ -0,0 +1,24 @@
+class apiError extends Error{
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        error =[],
+        statck = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = errors
+
+        if(this.stack){
+            this.stack = statck
+        }else{
+            Error.captureStackTrace(this,this.
+                constructor)
+        }
+    }
+}
+
+export {apiError}
